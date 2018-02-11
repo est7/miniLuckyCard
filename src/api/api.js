314f34fc0366ca3dfc7912778f2236d75757ab3d
lihang <lihang@droi.com>
2018-02-11 17:30:08 +0800

    提交

--- src/api/api.js
+++ src/api/api.js
@@ -8,9 +8,11 @@ let env = '-test' // -dev 或者 -test
 const getHomeList = (data) => getRequest('home', data)
 const getRootCtegoryList = (data) => getRequest('classifyList', data)
 const getChildGoodsCatetoryList = (data) => getRequest('classifyList', data)
+const getSubCatetoryDetailList = (data) => getRequest('classifyGoods', data)
 
 module.exports = {
   getHomeList,
   getRootCtegoryList,
-  getChildGoodsCatetoryList
+  getChildGoodsCatetoryList,
+  getSubCatetoryDetailList
 }
